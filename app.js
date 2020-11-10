34bf3cec1c06c63b3b8555265f40f691f8e29384
Nour0700 <58932402+Nour0700@users.noreply.github.com>
2020-11-10 20:21:23 +0300

    delete heroku

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const Telegraf = require('telegraf');
 const express = require("express");
 const app = express();
-// const Markup = require('telegraf/markup');
-// const Extra = require('telegraf/extra');
 const https = require("https");
 require('dotenv').config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -11,17 +9,6 @@ bot.start((ctx) => {
 	ctx.reply(`Hello ${ctx.from.first_name}, would you like to know the Weather? Please send the name of the city in which you want to know the weather`)
 })
 bot.help((ctx) => ctx.reply('Send me the name of the city you want to know its weather condition today.'))
-
-let port = process.env.PORT;
-if (port == null || port == "") {
-	port = 3000;
-}
-app.listen(port, function () { console.log("Server Started successfully"); });
-
-// bot.on('sticker', (ctx) => {
-// 	ctx.reply("bla bla"); 
-// })
-
 
 bot.on('text', (ctx) => {
 	const cityName = ctx.message.text;
@@ -52,24 +39,3 @@ bot.on('text', (ctx) => {
 	});
 });
 bot.launch();
-
-
-// bot.hears('Assalamualaikum', (ctx) => {
-// 	ctx.reply('Waalaikumsalam'); 
-// });
-// bot.hears('hello', (ctx) => {
-// 	ctx.reply('<b>Hello</b>. <i>How are you today?</i>',
-// 		Extra.HTML()
-// 			.markup(Markup.inlineKeyboard([
-// 				Markup.callbackButton('Not bad', 'not bad'),
-// 				Markup.callbackButton('All right', 'all right')
-// 			])));
-// });
-// bot.action('not bad', (ctx) => {
-// 	ctx.editMessageText('<i>Have a nice day 😊</i>',
-// 		Extra.HTML())
-// });
-// bot.action('all right', (ctx) => {
-// 	ctx.editMessageText('<i>May happiness be with you 🙏</i>',
-// 		Extra.HTML())
-// });
